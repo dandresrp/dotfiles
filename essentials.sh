cb3371402a448eadb0ecf7536cf5a1f943b6c0ea
Diego Rios <dandresrp00@gmail.com>
2021-07-13 12:18:21 -0600

    Updated essentials and xmobarrc

--- essentials.sh
+++ essentials.sh
@@ -5,7 +5,7 @@ sudo reflector --country 'United States' --age 12 --protocol https --sort rate -
 
 sudo pacman -Syy
 
-sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings vlc discord nitrogen picom rofi dmenu alacritty pcmanfm git libreoffice-fresh numlockx anki flameshot gimp gwenview notepadqq nomacs nvidia-settings lxappearance mlocate gvfs udisks2 arc-gtk-theme xterm polkit lxsession vifm
+sudo pacman -S --noconfirm xorg xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings vlc discord nitrogen picom rofi dmenu alacritty pcmanfm git libreoffice-fresh numlockx anki flameshot gimp gwenview notepadqq nomacs nvidia-settings lxappearance mlocate gvfs udisks2 arc-gtk-theme xterm polkit lxsession vifm
 
 sudo systemctl enable lightdm
 
@@ -16,6 +16,6 @@ makepkg -si --noconfirm
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
 curl -sS https://linux.dropbox.com/fedora/rpm-public-key.asc | gpg --import -
 
-paru -S --noconfirm visual-studio-code-bin google-chrome ttf-ms-fonts foxitreader zoom spotify dropbox mailspring notion-app elementary-planner notes
+paru -S --noconfirm visual-studio-code-bin google-chrome ttf-ms-fonts foxitreader zoom spotify dropbox mailspring notion-app notes
 
 sudo updatedb
