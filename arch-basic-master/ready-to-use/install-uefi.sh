8008e6fc6134a57e4bc83e715c04540c1c67121d
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2021-08-01 16:15:25 -0600

    Update install-uefi.sh

--- arch-basic-master/ready-to-use/install-uefi.sh
+++ arch-basic-master/ready-to-use/install-uefi.sh
@@ -2,7 +2,7 @@
 
 pacman -S grub efibootmgr dosfstools mtools os-prober
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S base-devel pacman-contrib git reflector nano dialog rsync linux-headers networkmanager xdg-user-dirs amd-ucode nvidia nvidia-settings ntfs-3g alsa-utils alsa-plugins pipewire pipewire-alsa pipewire-pulse pipewire-jack xdg-utils bash-completion openssh firewalld rust
