993b7d5e7b71698480da46ecb62218e86d152f11
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2021-08-02 21:09:29 -0600

    Update awesome.sh

--- arch-basic-master/ready-to-use/awesome.sh
+++ arch-basic-master/ready-to-use/awesome.sh
@@ -8,6 +8,8 @@ sudo firewall-cmd --reload
 
 sudo pacman -S xorg xorg-xinit awesome lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings vlc discord nitrogen picom rofi dmenu alacritty pcmanfm libreoffice-fresh numlockx anki gimp notepadqq lxappearance mlocate gvfs udisks2 xterm polkit lxsession noto-fonts-emoji hunspell-en_us hunspell-es_any hunspell-es_hn hunspell-es_mx hunspell-es_es firefox papirus-icon-theme exa bat fish nomacs flameshot neofetch
 
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
 chsh -s /bin/fish
 
 sudo systemctl enable lightdm
