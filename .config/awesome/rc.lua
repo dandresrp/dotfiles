e2ed80df0c30a56ec44e7d5fbe83e54b41bb5ea3
Diego Rios <dandresrp00@gmail.com>
2021-09-25 13:20:10 -0600

    update awesome, rofi

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -473,7 +473,7 @@ awful.rules.rules = {
                      keys = clientkeys,
                      buttons = clientbuttons,
                      screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen+awful.placement.centered
+		     placement = awful.placement.centered
      }
     },
 
