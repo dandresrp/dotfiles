dc1d358b4244f432c7915cbbf52ac297b57e2ed1
Diego Rios <dandresrp00@gmail.com>
2021-11-21 12:36:22 -0600

    update qtile config

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-picom &
+# Cursor active at boot
+xsetroot -cursor_name left_ptr &
+
+# Polkit agent
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Notification Daemon
+exec dunst &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
+
+# Enable power management
+xfce4-power-manager &
+
 nitrogen --restore &
-numlockx & 
-setxkbmap us -variant altgr-intl
+picom --config $HOME/.config/qtile/picom.conf &
+volumeicon &
+
+## Set keyboard layout
+setxkbmap us -variant altgr-intl &
+
+## Turn ON numlock key
+numlockx on &
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,48 +1,54 @@
-#################################
-######                     ######
-###### QTILE CONFIGURATION ######
-######                     ###### 
-#################################
-
-###################
-#### IMPORTS ######
-###################
-
-
-from typing import List  # noqa: F401
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import os
-import re
-import socket
 import subprocess
+from typing import List  # noqa: F401
+
 from libqtile import qtile
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
-###################
-#### VARIABLES ####
-###################
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.call([home])
+
 
 mod = "mod4"
-terminal = guess_terminal()
-applauncher     = "rofi -show drun"
-browser  = "google-chrome-stable"
-files    = "pcmanfm"
-
-#####################
-#### KEYBINDINGS ####
-#####################
+terminal = "alacritty -e fish"
+browser = "google-chrome-stable"
+launcher = "rofi -show drun"
+filemanager = "thunar"
+sessionmanager = "clearine"
 
 keys = [
-
-    # Launch 
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "p", lazy.spawn(applauncher), desc="Launch rofi"),
-    Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
-    Key([mod], "e", lazy.spawn(files), desc="Launch file manager"),
-
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
@@ -79,61 +85,94 @@ keys = [
     Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
 
+    # Launch programs
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
+    Key([mod], "e", lazy.spawn(filemanager), desc="Launch file manager"),
+    Key([mod], "p", lazy.spawn(launcher), desc="Launch app launcher"),
+    Key([mod], "x", lazy.spawn(sessionmanager), desc="Launch session manager"),
+
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
-
-    # Sound
-        Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),
-        Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -q -D pulse sset Master 5%-")),
-        Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -q -D pulse sset Master 5%+"))
 ]
 
-####################
-#### WORKSPACES ####
-####################
+groups = [Group(i) for i in "123456789"]
 
-group_names = [("WWW", {'layout': 'monadtall'}),
-              ("DEV", {'layout': 'monadtall'}),
-              ("DOC", {'layout': 'monadtall'}),
-              ("CHAT", {'layout': 'monadtall'}),
-              ("MUS", {'layout': 'monadtall'})]
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
 
-groups = [Group(name, **kwargs) for name, kwargs in group_names]
-
-for i, (name, kwargs) in enumerate(group_names, 1):
-    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))
-    keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name)))
-
-#################
-#### LAYOUTS ####
-#################
-
-layout_theme = {"border_width": 2,
-                "margin": 4,
-                "border_focus": "999",
-                "border_normal": "333"}
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
 
 layouts = [
-    #layout.MonadWide(**layout_theme),
-    #layout.Bsp(**layout_theme),
-    #layout.Stack(stacks=2, **layout_theme),
-    #layout.Columns(**layout_theme),
-    #layout.RatioTile(**layout_theme),
-    #layout.Tile(shift_windows=True, **layout_theme),
-    #layout.VerticalTile(**layout_theme),
-    #layout.Matrix(**layout_theme),
-    #layout.Zoomy(**layout_theme),
-     layout.MonadTall(**layout_theme),
-     layout.Max(**layout_theme),
-     layout.Stack(num_stacks=2),
-     layout.RatioTile(**layout_theme),
-     layout.Floating(**layout_theme)]
+    layout.Columns(border_focus='#676E95', border_width=1),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(active='#FFFFFF', inactive='#515772'),
+                # widget.Prompt(),
+                # widget.WindowName(),
+                widget.TaskList(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(icon_size=14, padding=6),
+                widget.CheckUpdates(
+                    update_interval=1800,
+                    distro="Arch_checkupdates",
+                    display_format="{updates} Updates",
+                    mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(
+                        terminal + ' -e sudo pacman -Syu')}
+
+                ),
+                widget.Clock(format=' %I:%M %p - %d/%m/%Y'),
+                widget.CurrentLayout(),
+                # widget.QuickExit(),
+            ],
+            size=20, opacity=1.0, background='#292D3E',
+        ),
+    ),
+]
 
 # Drag floating layouts.
 mouse = [
@@ -141,47 +180,11 @@ mouse = [
          start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())                ]
-
-#######################
-#### BAR & WIDGETS ####
-#######################
-
-widget_defaults = dict(
-    font='Cantarell',
-    fontsize=14,
-    padding=4,
-)
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    Screen(
-        top=bar.Bar( 
-            [
-                widget.GroupBox(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Systray(padding=10),
-                widget.Sep(),
-                widget.Volume(padding=10),
-                widget.Sep(),
-                widget.Clock(format="%d/%m/%Y | %I:%M %p", padding=10),
-               ],
-            28,
-            background="#282a36",
-            foreground="#f8f8f2",
-        ),
-    ),
+    Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
-main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
@@ -197,15 +200,11 @@ floating_layout = layout.Floating(float_rules=[
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
+reconfigure_screens = True
 
-###################
-#### AUTOSTART ####
-###################
-
-@hook.subscribe.startup_once
-def autostart():
-    home = os.path.expanduser('~/.config/qtile/autostart.sh')
-    subprocess.call([home])
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
@@ -215,3 +214,4 @@ def autostart():
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
+wmname = "LG3D"
